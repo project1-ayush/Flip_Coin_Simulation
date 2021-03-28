704b3ee4ca302eb539d193c83054fb7a61cc5813
project1-ayush <trivediayush491112@gmail.com>
2021-03-28 21:30:22 +0530

    Extend: Tie Case Condition

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
 Head=1
+win=0
 freqHead=0
 freqTail=0
-win=0
 
 while [[ $freqHead -lt 21 && $freqTail -lt 21 ]]
 do
@@ -29,4 +29,31 @@ then
 		echo "Tail Win By $win from Head"
 else
 		echo "Its a Tie"
+		
+		while [ $win -lt 2 ]
+		do
+				randomFlip=$((RANDOM%2))
+
+				if [ $randomFlip -eq $Head ]
+				then
+						((freqHead++))
+				else
+						((freqTail++))
+				fi
+
+
+							if [ $freqHead -gt $freqTail ]
+							then
+									win=$(( $freqHead - $freqTail ))
+									echo "Head win by $win"
+							elif [ $freqTail -gt $freqHead ]
+							then
+									win=$(( $freqTail - $freqHead ))
+									echo "Tail win by $win"
+							fi
+
+		done
+
 fi
+
+
