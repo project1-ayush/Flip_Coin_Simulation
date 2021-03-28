0841c80477aa16798091a5663856412d8a2dd8ad
project1-ayush <trivediayush491112@gmail.com>
2021-03-28 15:10:34 +0530

    Added: Multiple Times Flipped Coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,14 +2,22 @@
 
 Head=1
 Tail=0
+freqHead=0
+freqTail=0
+num=10
 
-randomFlip=$((RANDOM%2))
+while [ $num -ne 0 ]
+do
+	randomFlip=$((RANDOM%2))
 
-if [ $randomFlip -eq $Head ]
-	then
-		echo "Head Occurs"
-	else
-		echo "Tail Occurs"
-fi
+	if [ $randomFlip -eq $Head ]
+		then
+				((freqHead++))
+		else
+				((freqTail++))
+	fi
 
+	((num--))
+done
 
+echo "Head occurred $freqHead times. Tail occurred $freqTail times."
